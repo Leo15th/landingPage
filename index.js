65b76ff61a618fbf595dc925e6524a0eff3ca901
Leo15th <pwaiag2023@gmail.com>
2026-01-13 11:33:55 +0700

    update UI

--- index.js
+++ index.js
@@ -51,10 +51,6 @@ var swiper = new Swiper(".menuSwiper", {
     },
   },
   loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -68,19 +64,19 @@ const gallerySwiper = new Swiper(".gallerySwiper", {
   speed: 4000,
   allowTouchMove: true,
   centeredSlides:false,
-  slidesPerView: 1,
+  slidesPerView: 2,
   spaceBetween: 10,
   breakpoints: {
     640: {
-      slidesPerView: 2,
+      slidesPerView: 3,
       spaceBetween: 20,
     },
     768: {
-      slidesPerView: 3,
+      slidesPerView: 4,
       spaceBetween: 40,
     },
     1024: {
-      slidesPerView: 4,
+      slidesPerView: 5,
       spaceBetween: 50,
     },
   },
